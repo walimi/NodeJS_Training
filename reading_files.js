8d42f482581aff44e022726b3e61e93afa04ceea
Wahid Alimi <wahid.alimi@gmail.com>
2018-02-06 18:50:36 -0500

    Read a directory and log it's contents.

---
+++ reading_files.js
@@ -0,0 +1,26 @@
+
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+
+/**
+ * Reads files from directory and logs contents of the files 
+ */
+var printFileContent = function(directory) {
+    fs.readdir(directory, function(err, files){
+        files.forEach(function(fileName){
+            var file = path.join(__dirname, "lib", fileName);
+            var stats = fs.statSync(file);
+            if(stats.isFile()) {
+                fs.readFile(file, function(err, contents){
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(contents);
+                });
+            }
+        });    
+    });
+}
+
