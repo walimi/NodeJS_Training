789f84ee2e3fc56b69a6ebb3a29892344c2595d1
Wahid Alimi <wahid.alimi@gmail.com>
2018-02-12 19:17:14 -0500

    Building an HTTP server module.

---
+++ 06_HTTP_Module/server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { "Content-type": "text/html"});
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title> HTML Response</title>
+            </head>
+            <body>
+                <h1>Serving HTML</h1>
+                <p>${req.url}</p>
+                <p>${req.method}</p>
+            </body>
+        </html>
+    `);
+});
+
+
+server.listen(3000);
+
+console.log("server listening on port 3000");
