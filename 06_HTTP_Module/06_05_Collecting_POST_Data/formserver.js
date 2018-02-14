6f004fc3ba167dba863ba78467d702726063661c
Wahid Alimi <wahid.alimi@gmail.com>
2018-02-13 19:39:46 -0500

    06.05 - Collecting post data.

---
+++ 06_HTTP_Module/06_05_Collecting_POST_Data/formserver.js
@@ -0,0 +1,35 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(req, res) {
+
+    if (req.method === "GET") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream("./public/form.html", "UTF-8").pipe(res);
+    } else if (req.method === "POST") {
+        var body = "";
+
+        req.on("data", function(chunk) {
+            body += chunk;
+        });
+
+        req.on("end", function(){
+            res.writeHead(200, {"Content-type":"text/html"});
+            res.end(`
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <title>Form Results</title>
+                </head>
+                <body>
+                    <h1>Your form results</h1>
+                    <p>${body}</p>
+                </body>
+                </html>
+            `);
+        });
+    }
+
+}).listen(3000);
+
+console.log("Server listening on port 3000");
