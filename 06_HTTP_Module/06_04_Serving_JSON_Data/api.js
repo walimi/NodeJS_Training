5d3ce3ed68a8483f51f29ee811a1a5dace0271da
Wahid Alimi <wahid.alimi@gmail.com>
2018-02-13 19:25:20 -0500

    06.04 - Filter JSON data returned to client.

--- 06_HTTP_Module/06_04_Serving_JSON_Data/api.js
+++ 06_HTTP_Module/06_04_Serving_JSON_Data/api.js
@@ -5,9 +5,34 @@ var data = require("./data/inventory");
 
 http.createServer(function(req, res) {
     
-    res.writeHead(200, {"Content-type": "text/json"});
-    res.end(JSON.stringify(data));
-
+    if (req.url === "/") { // requesting all data
+        res.writeHead(200, {"Content-type": "text/json"});
+        res.end(JSON.stringify(data));
+    } else if (req.url === "/instock") { 
+        listInStock(res);
+    } else if (req.url === "/onbackorder") {
+        listOnBackOrder(res);
+    } else {
+        res.writeHead(404, {"Content-type": "text/plain"});
+        res.end("Whoops...Data not found.");
+    }
 }).listen(3000);
 
 console.log("Server listening on port 3000");
+
+function listInStock(res) {
+    var inStock = data.filter(function(item) {
+        return item.avail.toLowerCase().trim() === "in stock";
+    });
+
+    res.end(JSON.stringify(inStock));
+}
+
+function listOnBackOrder(res) {
+    var onBackOrder = data.filter(function(item) {
+        return item.avail.toLowerCase().trim() === "on back order";
+    });
+
+    res.end(JSON.stringify(onBackOrder));
+}
+
