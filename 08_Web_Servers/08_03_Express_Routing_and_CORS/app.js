359bb11dc7ff3fcad0e0856c119471861975e8e2
Wahid Alimi <wahid.alimi@gmail.com>
2018-02-22 19:24:01 -0500

    08_03 - Serving dictionary data via Express.

--- 08_Web_Servers/08_03_Express_Routing_and_CORS/app.js
+++ 08_Web_Servers/08_03_Express_Routing_and_CORS/app.js
@@ -1,7 +1,28 @@
 var express = require("express");
 var app = express();
 
-
+var soccerTerms = [
+	{
+		term: "ADVANTAGE RULE",
+		defined: "The referee does not have to stop play every time he detects a foul. "
+	},
+	{
+		term: "LINESMAN",
+		defined: "Former term for assistant referee."
+	},
+	{
+		term: "TACKLE",
+		defined: "To use the feet in attempting to take the ball from an opponent's feet."
+	},
+	{
+		term: "PENALTY",
+		defined: "Beware! The word 'penalty' has a very specific (and very dramatic) meaning in soccer. "
+	},
+	{
+		term: "OFFSIDE",
+		defined: "The word is singular. Offside. How or why the plural version 'offsides' arose I have no idea, but it is incorrect."
+	},
+];
 
 app.use(function(req, res, next) {
 	console.log(`${req.method} request for '${req.url}'`);
@@ -10,6 +31,9 @@ app.use(function(req, res, next) {
 
 app.use(express.static("./public"));
 
+app.get("/dictionary-api", function(req, res) {
+	res.json(soccerTerms);
+});
 
 app.listen(3000);
 
