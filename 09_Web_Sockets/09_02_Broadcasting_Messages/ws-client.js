9c696ab346d1f85c994ea0c4fd0dcd7cd7c6f423
Wahid Alimi <wahid.alimi@gmail.com>
2018-02-25 13:39:03 -0500

    09_02 - Broadcast messages with websockets.

--- 09_Web_Sockets/09_02_Broadcasting_Messages/ws-client.js
+++ 09_Web_Sockets/09_02_Broadcasting_Messages/ws-client.js
@@ -16,6 +16,7 @@ ws.onmessage = function(payload) {
 
 document.forms[0].onsubmit = function () {
     var input = document.getElementById('message');
+    ws.send(input.value);
     input.value = '';
 };
 
--- 09_Web_Sockets/09_02_Broadcasting_Messages/ws.js
+++ 09_Web_Sockets/09_02_Broadcasting_Messages/ws.js
@@ -3,6 +3,15 @@ var WebSocketServer = require("ws").Server;
 var wss = new WebSocketServer({ port: 3000 });
 
 wss.on("connection", function(ws) {
+    ws.on("message", function(message) {
+        if(message === "exit") {
+            wss.close();
+        } else {
+            wss.clients.forEach(function(client) {
+                client.send(message);
+            });
+        }
+    });
     ws.send("Welcome to cyber chat");
 });
 
