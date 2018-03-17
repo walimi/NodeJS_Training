fcc3e1b6521305feec08388c090aa418d70b0460
Wahid Alimi <wahid.alimi@gmail.com>
2018-03-17 06:12:33 -0400

    10_05 - Advanced testing with Sinon

--- 10_Testing_and_Debugging/10_05_Advanced_Testing_Sinon_Spies/test/order-spec.js
+++ 10_Testing_and_Debugging/10_05_Advanced_Testing_Sinon_Spies/test/order-spec.js
@@ -5,6 +5,8 @@ var rewire = require("rewire");
 // instead of require so that we can set the inventoryData variable
 var order = rewire("../lib/order");
 
+var sinon = require("sinon");
+
 describe("Ordering items", function() {
     
     // we execute this before each test function
@@ -15,15 +17,31 @@ describe("Ordering items", function() {
             { sku: "CCC", qty: 3 },
         ];
 
+        this.console = {
+            log: sinon.spy()
+        };
+
         // note that "inventoryData" is the name of the variable in order module
         // here, we're reseting it to this.testData    
         order.__set__("inventoryData", this.testData);
+
+        // we're "hijacking" the console.log statements so to avoid seeing those
+        // in our output. 
+        order.__set__("console", this.console);
 
     });
 
     it("order an item if there are enough in the stock", function(done){
         
+        // we need to protect the scope of this object
+        var _this = this;
+
         order.orderItem("CCC", 3, function() {
+
+            // this is to to ensure our console object's are called properly
+            // (at this point we should have called console.log 2 times)
+            expect(_this.console.log.callCount).to.equal(2);
+
             // if this callback function is invoked then it means the orderItem was 
             // successfull and the test will pass
             done();
